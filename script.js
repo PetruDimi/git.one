499b35f302d9ef4e754f2b401e4fe49fa646da9c
Petru.Dimi <dimitriu_petru@yahoo.com>
2023-09-13 00:54:40 +0300

    added a new script.js file (button that changes colors)

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('git tutotial');
+console.log("git tutotial");
+
+const button = document.createElement("button");
+button.innerHTML = "Click me";
+button.style = "background-color:red";
+
+const body = document.querySelector("body");
+body.appendChild(button);
+
+const changeColor = () => {
+
+    if (button.style.backgroundColor == 'red'){
+        button.style.backgroundColor = 'blue'
+        button.style.fontSize = '2em'
+    } else if (button.style.backgroundColor == 'blue'){
+        button.style.backgroundColor = 'green'
+        button.style.fontSize = '3em'
+
+    }
+    else if (button.style.backgroundColor == 'green'){
+        button.style.backgroundColor = 'red'
+        button.style.fontSize = '1em'
+    }
+};
+
+button.addEventListener("click", changeColor);
